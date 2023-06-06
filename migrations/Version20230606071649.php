5e1002b95c1eba9dcc1002fdd726a08dfe792ea5
Sergey Eremeev <eremeev87@bk.ru>
2023-06-06 15:37:08 +0800

    install fixtures

--- migrations/Version20230606071649.php
+++ migrations/Version20230606071649.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230605082843 extends AbstractMigration
+final class Version20230606071649 extends AbstractMigration
 {
     public function getDescription(): string
     {
