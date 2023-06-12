846760ae80fbdffb93bf35db809eff355e01c8b9
Sergey Eremeev <eremeev87@bk.ru>
2023-06-12 16:42:43 +0800

    Authorization works

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -3,11 +3,15 @@
 namespace App\Security;
 
 use App\Repository\UserRepository;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\Exception\BadCredentialsException;
+use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
+use Symfony\Component\Security\Csrf\CsrfTokenManagerInterface;
 use Symfony\Component\Security\Http\Authenticator\AbstractAuthenticator;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\CustomCredentials;
@@ -18,10 +22,12 @@ class LoginFormAuthenticator extends AbstractAuthenticator
 {
 
     private UserRepository $userRepository;
+    private CsrfTokenManagerInterface $csrfToken;
 
-    public function __construct(UserRepository $userRepository)
+    public function __construct(UserRepository $userRepository, CsrfTokenManagerInterface $csrfToken)
     {
         $this->userRepository = $userRepository;
+        $this->csrfToken = $csrfToken;
     }
 
     public function supports(Request $request): ?bool
@@ -33,6 +39,13 @@ class LoginFormAuthenticator extends AbstractAuthenticator
     {
         $email = $request->request->get('email');
         $password = $request->request->get('password');
+        $csrf_token = $request->request->get('_csrf_token');
+
+        $request->getSession()->set(
+            Security::LAST_USERNAME,
+            $email
+        );
+
         return new Passport(
             new UserBadge($email),
             new CustomCredentials(function ($credentials, User $user){
@@ -48,7 +61,13 @@ class LoginFormAuthenticator extends AbstractAuthenticator
 
     public function onAuthenticationFailure(Request $request, AuthenticationException $exception): ?Response
     {
-        dd('Пароль НЕ верный!');
+        //dd($exception);
+        if ($request->hasSession()) {
+            $request->getSession()->set(Security::AUTHENTICATION_ERROR, $exception);
+            //return throw new CustomUserMessageAuthenticationException('error custom ');
+        }
+        $url = '/login';
+        return new RedirectResponse($url);
     }
 
 //    public function start(Request $request, AuthenticationException $authException = null): Response
